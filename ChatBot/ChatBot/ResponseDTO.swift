48f565db889a57e2cb07dc1cdd0c8ce2f607dfce
nayeon <nayeon7612@gmail.com>
2024-03-27 20:28:14 +0900

    refactor: 응답 모델 수정

--- ChatBot/ChatBot/ResponseDTO.swift
+++ ChatBot/ChatBot/ResponseDTO.swift
@@ -5,24 +5,49 @@
 //  Created by dopamint on 3/26/24.
 //
 
-struct ResponseDTO: Codable {
+struct ResponseDTO: Decodable {
     let id: String
     let object: String
-    let created: String
+    let created: Int
     let model: String
-    let system_fingerprint: String
-    let choices: [ChoicesDTO]
-    let usage: [UsageDTO]
+    let systemFingerprint: String
+    let choices: [Choice]
+    let usage: Usage
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case object
+        case created
+        case model
+        case choices
+        case usage
+        case systemFingerprint = "system_fingerprint"
+    }
+
 }
 
-struct UsageDTO : Codable {
-    let prompt_tokens: Int
-    let completion_tokens: Int
-    let total_tokens: Int
+struct Usage: Decodable {
+    let promptTokens: Int
+    let completionTokens: Int
+    let totalTokens: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case promptTokens = "prompt_tokens"
+        case completionTokens = "completion_tokens"
+        case totalTokens = "total_tokens"
+    }
 }
 
-struct ChoicesDTO : Codable {
+struct Choice: Decodable {
     let index: Int
-    let message: Int
-    let finish_reason: String
+    let message: MessageDTO
+    let logprobs: String?
+    let finishReason: String
+    
+    enum CodingKeys: String, CodingKey {
+        case index
+        case message
+        case logprobs
+        case finishReason = "finish_reason"
+    }
 }
