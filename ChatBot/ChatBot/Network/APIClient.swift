1fcfe8509ccb57ea44a3112c267a2070f17572a0
angryeon7 <nayeon7612@gmail.com>
2024-04-04 10:25:01 +0900

    feat: 네트워크 요청 APIClient 구현

---
+++ ChatBot/ChatBot/Network/APIClient.swift
@@ -0,0 +1,39 @@
+//
+//  APIClient.swift
+//  ChatBot
+//
+//  Created by nayeon  on 4/3/24.
+//
+
+import Alamofire
+
+final class APIClient {
+    static let shared = APIClient()
+    
+    func request<T: Decodable>(_ object: T.Type,
+                               router: URLRequestConvertible,
+                               completion: @escaping (NetworkResult<T>) -> Void) {
+        AF.request(router)
+            .validate(statusCode: 200..<500)
+            .responseDecodable(of: object) { response in
+                switch response.result {
+                case .success(let decodedData):
+                    completion(.success(decodedData))
+                case .failure(_):
+                    if let statusCode = response.response?.statusCode {
+                        switch statusCode {
+                        case 400...499:
+                            completion(.failure(.pathError))
+                        case 500...599:
+                            completion(.failure(.serverError))
+                        default:
+                            completion(.failure(.networkFail))
+                        }
+                    } else {
+                        completion(.failure(.networkFail))
+                        
+                    }
+                }
+            }
+    }
+}
