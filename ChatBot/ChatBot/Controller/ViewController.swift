4b7736bf035bcc61a490c63b787b521cd92ae2cc
angryeon7 <nayeon7612@gmail.com>
2024-04-04 10:52:08 +0900

    feat: 요청 테스트

--- ChatBot/ChatBot/Controller/ViewController.swift
+++ ChatBot/ChatBot/Controller/ViewController.swift
@@ -8,7 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let chatService = ChatService()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        sendTestMessage("안녕 만나서 반가워")
+    }
+
+    func sendTestMessage(_ message: String) {
+        chatService.sendChatRequest(message: message) { result in
+            switch result {
+            case .success(let response):
+                print("Response: \(response)")
+            case .failure(let error):
+                print("Error: \(error)")
+            }
+        }
     }
 }
--- ChatBot/ChatBot/Util/AppConfig.swift
+++ ChatBot/ChatBot/Util/AppConfig.swift
@@ -9,7 +9,7 @@ import Foundation
 
 enum AppConfig {
     static var openAIAPIKey: String {
-        guard let key = Bundle.main.infoDictionary?["API_KEY"] as? String else {
+        guard let key = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String else {
             assertionFailure("API_KEY를 찾을 수 없음")
             return ""
         }
