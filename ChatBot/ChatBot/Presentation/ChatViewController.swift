6e828ca859551f312df3e906fca1f39e1033e0e2
nayeon <nayeon7612@gmail.com>
2024-04-22 04:13:08 +0900

    feat: rootViewController변경 및 필요없는 파일 삭제

---
+++ ChatBot/ChatBot/Presentation/ChatViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ChatViewController.swift
+//  ChatBot
+//
+//  Created by nayeon  on 4/15/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class ChatViewController: UIViewController {
+    
+}
+
